190d5e314b4f33f99c4a9a990e329682da31db99
Alex Ablanya <harlex1990@gmail.com>
2023-06-16 14:47:36 -0700

    removing q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Return 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
