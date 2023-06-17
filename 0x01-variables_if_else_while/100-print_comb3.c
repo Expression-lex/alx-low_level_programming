548a753463f6de9d7d6919d936a48c5e4e28628a
Alex Ablanya <harlex1990@gmail.com>
2023-06-17 01:53:38 -0700

    this is aanother brain work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '1'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			if (i != j)
+			if (i < j)
 			{
 				putchar(i);
 				putchar(j);
 
-			if (i != '9' || j != '8')
+			if (i != '8' || j != '9')
 			{
 				putchar(',');
 				putchar(' ');
