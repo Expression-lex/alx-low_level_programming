e33e4d748bb6d597772b46373cad5bc5174d7675
Alex Ablanya <harlex1990@gmail.com>
2023-06-17 03:27:43 -0700

    using continue

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,21 +12,17 @@ int main(void)
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j <= 99 ; j++)
+		for (j = i + 1; j <= 99 ; j++)
 		{
-			if (i != j)
-			{
-				putchar('0' + i/10);
-				putchar('0' + i%10);
-				putchar(' ');
-				putchar('0' + j/10);
-				putchar('0' + j%10);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+			if (i == 98 && j == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
