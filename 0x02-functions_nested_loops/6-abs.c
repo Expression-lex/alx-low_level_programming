ee2e3294422422bbccb0256f53a58cbbfc01509a
Alex Ablanya <harlex1990@gmail.com>
2023-06-20 12:20:52 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - Check for absolute value
+ * @r: An integer
+ * Description: All desc
+ * Return: absolute
+ */
+
+int _abs(int r)
+{
+	return (r > 0 ? r : -*r);
+}
