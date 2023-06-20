bdeda67306739f3790d26e5d6be812bea4818aee
Alex Ablanya <harlex1990@gmail.com>
2023-06-20 07:39:26 -0700

    this is push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _ islower - Checking lower caing
+ * _islower - Checking lower caing
  * Return: 0 if lower or 1 if postive
  * @c : is collecting
  * Descrpition: No desc
- * Return: Always 0
  *
  */
 
@@ -14,7 +13,10 @@ int _islower(int c)
 
 	for (ca = 'a'; ca <= 'z'; ca++)
 	{
-		c == ca ? return(1) : return(0);
+		if (c == ca)
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
