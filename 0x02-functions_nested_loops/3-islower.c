f9691b42d09df6a80d51de9c653f0a576e777466
Alex Ablanya <harlex1990@gmail.com>
2023-06-20 07:25:24 -0700

    This a trial

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _ islower - Checking lower caing
+ * Return: 0 if lower or 1 if postive
+ * @c : is collecting
+ * Descrpition: No desc
+ * Return: Always 0
+ *
+ */
+
+int _islower(int c)
+{
+	char ca;
+	int d;
+
+	for (ca = 'a'; ca <= 'z'; ca++)
+	{
+		return (ca == c ? '1' : '0');
+	}
+}
