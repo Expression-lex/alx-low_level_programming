a0abee091def78507ac7816aeac00bee377bbdec
Alex Ablanya <harlex1990@gmail.com>
2023-06-20 06:17:12 -0700

    This is using while

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point to print alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char = i;
+	int = d;
+
+	while (d <= 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		d += 1
+		_putchar('\n');
+	}
+}
