9f761a765b7d2f8664f52e044291cf8f829c80e7
Alex Ablanya <harlex1990@gmail.com>
2023-06-20 08:03:27 -0700

    trying this

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - checks for higher casing
+ * Return: 1 if alpha and 1 if postive
+ * @c: is collecting
+ * *Description: None yet
+ *
+ */
+
+int _isalpha(int c)
+{
+	char alpha;
+
+	if (_islower(c) == 1)
+		return (1);
+	break;
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		if (c == alpha)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
