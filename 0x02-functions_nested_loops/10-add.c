65464c3c6437939a5527e9e21368594d4514eb1c
Alex Ablanya <harlex1990@gmail.com>
2023-06-21 14:55:59 -0700

    this is to add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Entry point for add
+ * @a: a is first int
+ * @b: b is second int
+ * Return: Return a+b
+ *
+ */
+
+int add(int a, int b)
+{
+	return ("%d", a + b);
+}
