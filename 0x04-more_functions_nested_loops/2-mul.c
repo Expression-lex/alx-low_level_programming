a096a45585896e7b9671276b91cd262e8a9a18ed
Alex Ablanya <harlex1990@gmail.com>
2023-06-22 08:43:43 -0700

    to multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point of the multiplication function
+ * @a: this take integer values for check
+ * @b: this take integer values for check
+ * Description: the fumction take two args and multiplly them to return 0
+ * Return: Always return 0
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
