0c24f6a276959201e28928b11bbf09d9b1db78d2
Expression-lex <Harlex1990@gmail.com>
2023-06-26 23:34:08 +0000

    this would swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Entry point
+ * Description: This function id for swap
+ * @a: Parameter to take first integer
+ * @b: Parameter to take next integer
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
