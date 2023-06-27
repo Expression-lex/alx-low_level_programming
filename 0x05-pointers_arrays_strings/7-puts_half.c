e397d054fab61b6290be5dd5018b1dadf25e46ed
Expression-lex <Harlex1990@gmail.com>
2023-06-27 16:21:20 +0000

    median

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,11 +14,11 @@ void puts_half(char *str)
 		len++;
 
 	if (len % 2 != 0)
-		n = (len - 1) / 2;
+		n = (len + 1) / 2;
 	else
 		n = len / 2;
 
-	for (i = n; i < len - 1; i++)
+	for (i = n; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
