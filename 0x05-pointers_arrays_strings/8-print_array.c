c70a4af84edfd7eddef5c03101829e7a47169e67
Expression-lex <Harlex1990@gmail.com>
2023-06-27 16:49:38 +0000

    list

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - Entry point
+ * @a: parameter input for array
+ * @n: Parameter for list
+ * Description: list out an array
+ * Result: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i <= n; i++)
+	{
+		_putchar(a[i]);
+		if (i == n)
+			continue;
+		_putchar('\,');
+		_putchar('\0');
+	}
+	_putchar('\n');
+}
