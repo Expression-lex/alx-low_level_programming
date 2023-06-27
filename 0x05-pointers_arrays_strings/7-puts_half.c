a2cfc43b8d88355ca7578a6871108689f5b48dad
Expression-lex <Harlex1990@gmail.com>
2023-06-27 16:13:16 +0000

    median

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - Entry point
+ * @str: Parameter function
+ * Description: This would print median
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 != 0)
+		n = (len - 1) / 2;
+	else
+		n = len / 2;
+
+	for (i = n; i <= len - 1; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
