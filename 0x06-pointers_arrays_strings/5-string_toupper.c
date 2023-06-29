5ac1eba04eec2435b22694cb5d38c135787668ae
Expression-lex <Harlex1990@gmail.com>
2023-06-29 20:01:11 +0000

    lowercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - convert all lowercase of a strinf
+ * @s: an input string
+ * Return: char pointer to converted string
+ */
+char *string_toupper(char *)
+{
+	char *new = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 32;
+		s++;
+	}
+	return (new);
+}
