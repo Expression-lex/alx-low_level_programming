244daac4257f635d83a3984c5697844bfc796a07
Expression-lex <Harlex1990@gmail.com>
2023-07-04 23:44:35 +0000

    pointing

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ * @s: double pointer
+ * @to: character to point
+ * Return: Nothing
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
