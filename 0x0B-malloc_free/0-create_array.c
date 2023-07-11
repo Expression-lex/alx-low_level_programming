2d63976bfd22dc6e82f9115392e4e540f5325572
Expression-lex <Harlex1990@gmail.com>
2023-07-11 16:18:47 +0000

    commiting to get char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - A function that creates an array of chars
+ * @size: The size of the array
+ * @c: initialized an array
+ * Return: A pointer to the array of Null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *new;
+	unsigned int i = 0;
+
+	new = malloc(size * sizeof(char));
+
+	if (new == NULL)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+	while (i < size)
+	{
+		new[i] = c;
+		i++;
+	}
+	return (new);
+}
