b180c9d00dd625f54675a8cc3f2d91cab9dbbbec
Expression-lex <Harlex1990@gmail.com>
2023-07-12 01:09:13 +0000

    split into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - A function that splits a string into words
+ * @str: An input pointer of the string to split
+ * Return: A pointer concatened strings
+ */
+char **strtow(char *str)
+{
+	char **arr;
+	int i = 0, j, m, k = 0, len = 0, count = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (; str[i]; i++)
+	{
+		if ((str[i] != ' ' || *str != '\t') &&
+		    ((str[i + 1] == ' ' ||
+		      str[i + 1] == '\t') || str[i + 1] == '\n'))
+			count++;
+	}
+	if (count == 0)
+		return (NULL);
+	arr = malloc(sizeof(char *) * (count + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0' && k < count; i++)
+	{
+		if (str[i] != ' ' || str[i] != '\t')
+		{
+			len = 0;
+			j = 1;
+			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
+				j++, len++;
+			arr[k] = malloc((len + 1) * sizeof(char));
+			if (arr[k] == NULL)
+			{
+				for (k = k - 1; k >= 0; k++)
+					free(arr[k]);
+				free(arr);
+				return (NULL);
+			}
+			for (m = 0; m < len; m++, i++)
+				arr[k][m] = str[i];
+			arr[k++][m] = '\0';
+		}
+	}
+	array[k] = NULL;
+	return (arr);
+}
