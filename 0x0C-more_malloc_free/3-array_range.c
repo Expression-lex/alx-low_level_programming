4cd74816b9a1b934cd8cfcd38321b080edefd29c
Expression-lex <Harlex1990@gmail.com>
2023-07-13 20:23:38 +0000

    array loc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: first integer
+ * @max: second integer
+ * Return: pointer to newly allocated memory
+ */
+
+int *array_range(int min, int max)
+{
+	int i, l, *new;
+
+	if (min > max)
+		return (NULL);
+	l = max - min + 1;
+	new = malloc(sizeof(int) * 1);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < l; i++, m++)
+	{
+		new[i] = min;
+	}
+	return (new);
+}
