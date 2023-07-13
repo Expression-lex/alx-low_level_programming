a14d57db483ab206c5f5d10410933148aa2225c4
Expression-lex <Harlex1990@gmail.com>
2023-07-13 19:20:27 +0000

    concat

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,3 @@
 char _putchar(int c);
 void *malloc_checked(unsigned int b);
-<<<<<<< HEAD
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-=======
->>>>>>> b459b18c94a9f309ee7a0c66481a1d98819f58e9
